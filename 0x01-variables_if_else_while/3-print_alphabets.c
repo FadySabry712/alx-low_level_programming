8a414cc3250421c47c406429baf9cdc63bfa5728
FadySabry712 <fadysabry95@yahoo.com>
2024-06-24 03:28:33 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ * Return: always 0
+ */
+
+int main(void)
+{
+        char a;
+
+	char b;
+
+        a = 'a';
+	b = 'A';
+        while
+                (a <= 'z') {
+		       	putchar(a);
+                        a++;
+                }
+	while
+		(b <= 'Z') {
+			putchar(b);
+			b++;
+		}
+        putchar('\n');
+return (0);
+}
