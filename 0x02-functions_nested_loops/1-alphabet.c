d2334d0d9636f6f91f0c817d7e5bfb8fa3088334
FadySabry712 <fadysabry95@yahoo.com>
2024-06-26 02:28:44 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main -entery point
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+char ch;
+
+ch = 'a';
+
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+return (0);
+}
