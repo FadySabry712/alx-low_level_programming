0d18c90321aa2d23dcc802aad98a12480dd41a8d
FadySabry712 <fadysabry95@yahoo.com>
2024-06-28 01:22:45 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - multiplys two numbers
+* @a: parameter
+* @b: parameter
+* Return: 1 if true 0 if not
+*/
+
+int mul(int a, int b)
+{
+int multiply;
+
+multiply = a * b;
+return (multiply);
+}
