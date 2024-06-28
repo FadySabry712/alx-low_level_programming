7929ff9331fdd45600713cb5da8da413b7107ece
FadySabry712 <fadysabry95@yahoo.com>
2024-06-28 01:26:21 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers from 0 to 9
+* Return: 1 if true 0 if not
+*/
+
+void print_numbers(void)
+
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
