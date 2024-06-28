1b0f7543f5d6b60a953d48c378670785dc1f0ba6
FadySabry712 <fadysabry95@yahoo.com>
2024-06-28 06:39:57 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
 * print_numbers - print numbers from 0 to 9
-* Return: 1 if true 0 if not
+* Return: 0
 */
 
 void print_numbers(void)
 
 {
 int c;
-for (c = 0; c < 10; c++)
+for (c = 48; c < 58; c++)
 {
 _putchar(c);
 }
