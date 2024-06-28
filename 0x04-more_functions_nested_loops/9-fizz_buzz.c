0e0b12ec6c0f691c5e43e7ee14bfbe2b7692bc2c
FadySabry712 <fadysabry95@yahoo.com>
2024-06-28 04:40:42 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints no from 1-100 * 3
+ * Return: 0
+ */
+
+int main(void)
+{
+int no;
+
+for (no = 1; no <= 100; no++)
+{
+if ((no % 3) == 0 && (no % 5) == 0)
+{
+	printf("FizzBuzz");
+}
+else if ((no % 3) == 0)
+{
+	printf("Fizz");
+}
+else if ((no % 5) == 0)
+{
+	printf("Buzz");
+}
+else
+{
+printf("%d", no);
+}
+if (no == 100)
+{
+	continue;
+	printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
