ca7d033e7588e43f3824b366dd947cffa89edad8
FadySabry712 <fadysabry95@yahoo.com>
2024-06-28 04:05:48 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * more_numbers - print any nymber 10 times
+ * Return: numbers
+ */
+
+void more_numbers(void)
+{
+
+	int i, c;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c >= 10)
+			{
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
