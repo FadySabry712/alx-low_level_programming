008bffddbfca8643aa91b5b78b8c775c4398d133
FadySabry712 <fadysabry95@yahoo.com>
2024-06-29 12:04:12 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main -entery point
+*Return: 0
+*/
+
+int main(void)
+{
+int b;
+
+for (b  = 0; b < 10; b++)
+{
+putchar(b + '0');
+if (b < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
