71006c754fa52aea1034c7780989e43ac6370c7b
FadySabry712 <fadysabry95@yahoo.com>
2024-07-01 20:36:27 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*reset_to_98 - derefence var memory value
+*@n: var to be refrenced
+*Return: refrenced value
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this where to learn all about pointers and arrays in c
