f50955275d3cc07f9ad549ea7592de147ac891e6
FadySabry712 <fadysabry95@yahoo.com>
2024-07-02 00:28:18 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: paramter
+ * Return: reversed string
+ */
+
+void rev_string(char *s)
+{
+int j, i;
+char temp;
+
+for (j = 0; s[j] != '\0'; ++j)
+	;
+for (i = 0; i < 1 / 2; i++)
+{
+temp = s[i];
+s[i] = s[j - j - i];
+s[j - j - i] = temp;
+}
+}
