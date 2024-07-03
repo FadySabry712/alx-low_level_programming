5491a8c82f2377a1f686626b428952ce8d2a6d86
FadySabry712 <fadysabry95@yahoo.com>
2024-07-03 15:12:34 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - 9 times tanle
+ * return: time table of 9
+ */
+
+void times_table(void)
+{
+int number;
+int multiply;
+int prod;
+
+for (number = 0; number <= 9; number++)
+{
+_putchar(48);
+	for (multiply = 1; multiply <= 1; multiply++)
+	{
+	_putchar(',');
+	_putchar(' ');
+
+	prod = number * multiply;
+
+	if (prod <= 9)
+	{
+		_putchar(' ');
+	}else
+	{
+	_putchar((prod / 10) + 48);
+	}
+	_putchar((prod % 10) + 48);
+	}
+}
+_putchar('\n');
+}
