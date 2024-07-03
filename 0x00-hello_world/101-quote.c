67ef5fb350e79e5993cf2308dac254fff67c8cbc
FadySabry712 <fadysabry95@yahoo.com>
2024-07-03 13:49:13 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unisrd.h>
+
+/**
+ * main- entery
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (0);
+}
