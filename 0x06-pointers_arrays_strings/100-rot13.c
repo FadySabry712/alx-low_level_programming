d3e3d14bfa115b862035a78216bc2311518d7614
FadySabry712 <fadysabry95@yahoo.com>
2024-07-05 20:55:05 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @s: param 1
+ * Return: encoded str
+ */
+
+char *rot13(char *s)
+{
+        int i;
+        char rot[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ";
+        char ROT[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIKLM";
+        char *ptr = s;
+
+        while (*s)
+        {
+                for (i = 0; i <= 52; i++)
+                {
+                        if (*s == rot[i])
+                        {
+                        *s = ROT[i];
+                        break;
+                        }
+                }
+                s++;
+        }
+        return (ptr);
+} 
