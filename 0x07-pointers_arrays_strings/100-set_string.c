5044962e03cd26c1faef129abadc3dbb53a33d6a
FadySabry712 <fadysabry95@yahoo.com>
2024-07-09 20:11:26 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set a value of a pointer to vhar 
+ * @**s: param 1
+ * @*to: param 1
+ * Return: the value of char pointed to
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
