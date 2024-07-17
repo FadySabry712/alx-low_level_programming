5dda1c729b6b9fdb96f049c5019d2187eee2b539
FadySabry712 <fadysabry95@yahoo.com>
2024-07-17 14:44:44 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main entery point
+ * @arg: param `1
+ * @argv: param 2
+ * Return: 0 Success
+ */
+
+int main(int arg, char const *argv[])
+{
+(void)arg;
+printf("%s\n", argv[0]);
+	return (0);
+}
