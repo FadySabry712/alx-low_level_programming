c3d20e6a9a33d15607d72f83930d94344eb36b11
FadySabry712 <fadysabry95@yahoo.com>
2024-07-17 14:50:51 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ * @argc: param 1
+ * @argv: param 2
+ * Return: 0 Success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+
+}
