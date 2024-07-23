b433cba0633baabec1f8fa80399ffaeb7489ca6b
FadySabry712 <fadysabry95@yahoo.com>
2024-07-22 20:08:37 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - main entery point
+* @name: name of dog
+* @age: age of dog
+* @owner: dog's owner nmae
+* Descriptio: degine some dog params :)
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif /* end if*/
