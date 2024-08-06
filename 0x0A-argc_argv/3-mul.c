1e46d4360f31dcacfd6f996314ba3d81a4432e4f
FadySabry712 <fadysabry95@yahoo.com>
2024-08-06 06:02:04 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * main - prints two sms og numbs
+ * @argc: param 1
+ * @argv: crab 2
+ * Return: 0 *successs>
+ */
+
+int main(int argc, char *argv[])
+{
+	int s;
+
+	if (argc == 3)
+	{
+		s = atoi(argv[1]) * atoi((argv[2]));
+		printf("%d\n", s);
+	}
+	else
+	{
+		printf("erro\n");
+		return (1);
+	}
+		return (0);
+}
