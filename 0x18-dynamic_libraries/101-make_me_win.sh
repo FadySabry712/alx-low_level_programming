7f03965f61404baa34b3c18d63547e72e83bfc9a
FadySabry712 <fadysabry95@yahoo.com>
2024-09-27 20:17:00 -0700

    Dyamic libraries added

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -P /tmp https://github.com/FadySabry712/alx/raw/refs/heads/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+this is to learn more about dynamic libraries in c
